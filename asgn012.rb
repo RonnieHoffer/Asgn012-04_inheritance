91c1584809cad88ff7a1c9ea60fc0e6ab59cdaa1
RonnieHoffer <ronnie.hoffer@gmail.com>
2021-02-01 20:20:04 -0500

    Step 5

--- asgn012.rb
+++ asgn012.rb
@@ -3,10 +3,12 @@ require_relative 'student'
 
 alpha = Instructor.new("Ronnie", "Hoffer")
 alpha.instructor_says
+alpha.greeting
 p alpha
 puts "\n"
 
 beta = Student.new("Darryl", "Hoffer")
 beta.student_says
+beta.greeting
 p beta
 puts "\n"
--- person.rb
+++ person.rb
@@ -6,4 +6,8 @@ class Person
         @first_name = first_name
         @last_name = last_name
     end
+
+    def greeting
+        puts "Hi, my name is #{@first_name} #{@last_name}."
+    end
 end
